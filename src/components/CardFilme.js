eaccfccfd705ff4ea35abe327daba715c18405ea
Victor Boehm <victor.b.assumpcao@aluno.senai.br>
2026-03-27 14:28:58 -0300

    genializa o vscode

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useFavoritos } from '../context/FavoritosContext';
